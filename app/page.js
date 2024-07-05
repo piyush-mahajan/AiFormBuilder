180cebb31850f6a0599f9428ce3a7fbaac8c9590
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-07-05 13:04:39 +0530

    Landing page and Logo

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Hero } from "./_components/Hero";
 
 export default function Home() {
   return (
-    <>
-      <Button>HI</Button>
-    </>
+    <div>
+      <Hero />
+    </div>
   );
 }
