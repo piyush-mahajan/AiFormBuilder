1dcf8f90419deafd0e058c2b3baf0f9ccc0f79fa
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-07-08 16:33:45 +0530

    drizzle orm and Neon postgress db

--- app/dashboard/_components/SideNav.jsx
+++ app/dashboard/_components/SideNav.jsx
@@ -5,7 +5,11 @@ import {
   Shield,
   icons,
 } from "lucide-react";
-import React from "react";
+import { usePathname } from "next/navigation";
+import { Progress } from "../../../@/components/ui/progress";
+import { Button } from "../../../@/components/ui/button";
+
+import React, { useEffect } from "react";
 
 const SideNav = () => {
   const menuList = [
@@ -13,39 +17,60 @@ const SideNav = () => {
       id: 1,
       name: "My forms ",
       icons: LibraryBig,
-      path: "/",
+      path: "/dashboard",
     },
     {
       id: 1,
       name: "responces  ",
       icons: MessageSquare,
-      path: "/",
+      path: "/dashboard/responses",
     },
     {
       id: 1,
       name: "analytics",
       icons: LineChart,
-      path: "/",
+      path: "/dashboard/analytics",
     },
     {
       id: 1,
       name: "Upgrade",
       icons: Shield,
-      path: "/",
+      path: "/dashboard/upgrade",
     },
   ];
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, [path]);
+
   return (
     <div className="h-screen shadow-md border bg-white ">
       <div className="p-5">
         {menuList.map((menu, index) => (
           <h2
             key={index}
-            className="flex items-center gap-3 p-4 hover:bg-primary hover:text-white rounded-lg"
+            className={`flex items-center gap-3 p-4 hover:bg-primary cusrosr-pointer hover:text-white rounded-lg ${
+              path == menu.path && "bg-primary text-white"
+            }`}
           >
             <menu.icons size={24} />
             {menu.name}
           </h2>
         ))}
+      </div>
+      <div className="fixed bottom-7 p-6 w-64">
+        <Button variant="outline" className="w-full">
+          + Create Form
+        </Button>
+        <div className="my-7">
+          <Progress value={33}></Progress>
+          <h2 className="text-sm mt-2 text-grey-600">
+            <strong>2</strong> out of <strong> 5</strong> being used{" "}
+          </h2>
+          <h2 className="text-sm mt-3 text-grey-600">
+            upgrade your plan for unlimited AI forms
+          </h2>
+        </div>
       </div>
     </div>
   );
